f4889a3e602dec618a73fee78ce341b99d680c5e
Dan <Dan@BB8>
2023-04-18 19:58:12 -0500

    Fixed a minor bug when building the To Convert folders

--- run_ffmpeg/src/run_ffmpeg/ExtractPGSFromMKVs.java
+++ run_ffmpeg/src/run_ffmpeg/ExtractPGSFromMKVs.java
@@ -435,7 +435,7 @@ public class ExtractPGSFromMKVs extends Thread
 	public void setChainA()
 	{
 		List< String > drivesAndFoldersToProbe = new ArrayList< String >() ;
-		drivesAndFoldersToProbe.addAll( common.addToConvertToEachDrive( common.getAllChainAMKVDrivesAndFolders() ) ) ;
+		drivesAndFoldersToProbe.addAll( common.addToConvertToEachDrive( common.getAllChainAMKVDrives() ) ) ;
 		drivesAndFoldersToProbe.addAll( common.getAllChainAMKVDrivesAndFolders() ) ;
 		setDrivesAndFoldersToExtract( drivesAndFoldersToProbe ) ;
 	}
@@ -446,7 +446,7 @@ public class ExtractPGSFromMKVs extends Thread
 	public void setChainB()
 	{
 		List< String > drivesAndFoldersToProbe = new ArrayList< String >() ;
-		drivesAndFoldersToProbe.addAll( common.addToConvertToEachDrive( common.getAllChainBMKVDrivesAndFolders() ) ) ;
+		drivesAndFoldersToProbe.addAll( common.addToConvertToEachDrive( common.getAllChainBMKVDrives() ) ) ;
 		drivesAndFoldersToProbe.addAll( common.getAllChainBMKVDrivesAndFolders() ) ;
 		setDrivesAndFoldersToExtract( drivesAndFoldersToProbe ) ;
 	}
