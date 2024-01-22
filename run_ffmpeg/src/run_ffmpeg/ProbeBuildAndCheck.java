7ddceca59faa742bc9391050581eb12df8c55d3a
Dan <Dan@BB8>
2024-01-21 14:23:32 -1000

    Added quick class to run ProbeDirectories BuildMovieAndShowIndexc and CheckLogicalIntegrity in succession

---
+++ run_ffmpeg/src/run_ffmpeg/ProbeBuildAndCheck.java
@@ -0,0 +1,15 @@
+package run_ffmpeg;
+
+/**
+ * The purpose of this class is to probe the directory structure, build
+ *  the database entries, and check the database for inconsistencies and duplicates.
+ */
+public class ProbeBuildAndCheck
+{
+	public static void main( String[] args )
+	{
+		ProbeDirectories.main( null ) ;
+		BuildMovieAndShowIndex.main( null ) ;
+		CheckLogicalIntegrity.main( null ) ;
+	}
+}
