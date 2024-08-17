716e7551b9a392d5eee3eaacd6b4594b9213b937
HerrNerdstromDRK <coding@karrels.com>
2024-08-16 14:49:50 -1000

    Config change

--- run_ffmpeg/src/run_ffmpeg/TranscodeAndMoveFiles.java
+++ run_ffmpeg/src/run_ffmpeg/TranscodeAndMoveFiles.java
@@ -80,6 +80,7 @@ public class TranscodeAndMoveFiles extends run_ffmpegControllerThreadTemplate< T
 		// Populate the list of folders to transcode.
 		List< String > foldersToTranscode = new ArrayList< String >() ;
 
+		
 //		foldersToTranscode.addAll( common.addMoviesAndTVShowFoldersToEachDrive( common.getAllMKVDrives() ) ) ;
 //		foldersToTranscode.add( "\\\\yoda\\\\MKV_Archive10\\To Convert\\Harold And Kumar Escape From Guantanamo Bay (2008)" ) ;
 		foldersToTranscode.addAll( common.addToConvertToEachDrive( common.getAllMKVDrives() ) ) ;
